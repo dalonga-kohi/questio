7d2236b8888b5085b86e0f9cba54cc44dc681324
seba-the-programmer <megaseba9@gmail.com>
2023-09-01 19:07:13 +0200

    Add toggle theme on reload

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,9 @@
+import useDarkMode from '../hooks/useDarkMode'
 import MainPage from '../pages/MainPage'
 import './index.css'
 
 function App() {
+  useDarkMode()
   return (
     <>
       <MainPage />
