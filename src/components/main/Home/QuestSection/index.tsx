6be458c8d3b54cae0630b18b6da310e267682542
seba-the-programmer <megaseba9@gmail.com>
2023-09-02 21:54:15 +0200

    Add QuestSection styling

--- src/components/main/Home/QuestSection/index.tsx
+++ src/components/main/Home/QuestSection/index.tsx
@@ -1,14 +1,22 @@
-type Category = 'latest' | 'popular'
+import { NavLink } from 'react-router-dom'
+
+type Category = 'latest' | 'popular' | 'recommended' | ''
 
 interface IQuestProps {
   category: Category
   title: string
 }
 
-const QuestSection = ({ title }: IQuestProps) => {
+const QuestSection = ({ category, title }: IQuestProps) => {
   return (
-    <section>
+    <section className="flex w-full justify-between items-center">
       <h2>{title}</h2>
+      <NavLink
+        to={`/category/${category}`}
+        className="text-white-light font-bold text-lg"
+      >
+        See More
+      </NavLink>
     </section>
   )
 }
