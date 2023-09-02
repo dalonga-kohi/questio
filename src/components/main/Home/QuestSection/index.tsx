56d6b688ef986f15da50f6f28e7a0621d54add38
seba-the-programmer <megaseba9@gmail.com>
2023-09-02 21:20:20 +0200

    Add QuestSection props logic

--- src/components/main/Home/QuestSection/index.tsx
+++ src/components/main/Home/QuestSection/index.tsx
@@ -1,5 +1,16 @@
-const QuestSection = () => {
-  return <></>
+type Category = 'latest' | 'popular'
+
+interface IQuestProps {
+  category: Category
+  title: string
+}
+
+const QuestSection = ({category, title}: IQuestProps) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+    </section>
+  )
 }
 
 export default QuestSection
--- src/components/main/Home/index.tsx
+++ src/components/main/Home/index.tsx
@@ -3,8 +3,7 @@ import QuestSection from './QuestSection'
 const Home = () => {
   return (
     <div className="dark:bg-black min-h-screen bg-gray-300 rounded-t-3xl px-6 py-4">
-      HI
-      <QuestSection />
+      <QuestSection category="latest" title="Most Popular 🔥🔥"/>
     </div>
   )
 }
