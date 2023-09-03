60dcb107c47bc467922537fca3d7a75fb00483f0
seba-the-programmer <megaseba9@gmail.com>
2023-09-03 18:47:13 +0200

    Add home page scrolling behaviour

--- src/components/main/Home/index.tsx
+++ src/components/main/Home/index.tsx
@@ -2,7 +2,7 @@ import QuestSection from './QuestSection'
 
 const Home = () => {
   return (
-    <div className="dark:bg-black min-h-screen bg-gray-300 rounded-t-3xl px-6 md:px-10 py-8">
+    <div className="dark:bg-black bg-gray-300 rounded-t-3xl px-6 md:px-10 py-8 overflow-y-auto max-h-screen">
       <QuestSection category="popular" title="Most Popular 🔥" />
       <QuestSection category="recommended" title="For You 😁" />
       <QuestSection category="latest" title="Latest 🎉" />
