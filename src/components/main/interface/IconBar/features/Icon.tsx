7e7b13aef29f5d45b4828e3b8c97764523e13c6c
seba-the-programmer <megaseba9@gmail.com>
2023-09-03 20:22:15 +0200

    Add icon bar features

--- src/components/main/interface/IconBar/features/Icon.tsx
+++ src/components/main/interface/IconBar/features/Icon.tsx
@@ -10,9 +10,12 @@ interface IIconProps {
 
 const Icon = ({ icon, link, caption }: IIconProps) => {
   return (
-    <NavLink to={link} className="">
-      <FontAwesomeIcon icon={icon} />
-      <div className="">{caption}</div>
+    <NavLink
+      to={link}
+      className="flex flex-col dark:text-white-light text-slate-700 justify-start items-center hover:bg-gray-400 dark:hover:bg-black rounded-md w-1/4 py-2 transition-colors"
+    >
+      <FontAwesomeIcon icon={icon} className="text-2xl dark:text-white" />
+      <div className="font-bold">{caption}</div>
     </NavLink>
   )
 }
