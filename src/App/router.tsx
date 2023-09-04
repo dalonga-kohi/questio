ed11ece244d07f6f978dbb887a53e2d549557e1d
seba-the-programmer <megaseba9@gmail.com>
2023-09-04 13:24:22 +0200

    Add search page to routing

--- src/App/router.tsx
+++ src/App/router.tsx
@@ -1,11 +1,17 @@
 import MainPage from '../pages/MainPage'
 import ErrorPage from '../pages/ErrorPage'
 import { createBrowserRouter } from 'react-router-dom'
+import SearchPage from '../pages/SearchPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/search',
+    element: <SearchPage />,
     errorElement: <ErrorPage />,
   },
 ])
