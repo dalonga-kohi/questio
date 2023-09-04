e02c66e210476668974443afd4ca5ab6b124e966
seba-the-programmer <megaseba9@gmail.com>
2023-09-04 11:45:32 +0200

    Fix error page responsivnes

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export default function ErrorPage() {
   return (
     <MainLayout>
-      <div className="flex flex-col w-full justify-center items-center h-96">
+      <div className="flex flex-col w-full justify-start pt-20 text-center items-center h-screen">
         <h2>Oops!</h2>
         <p className="mt-2 mb-4">Sorry, page you looking for does not exist.</p>
         <NavLink to="/" className="link">
