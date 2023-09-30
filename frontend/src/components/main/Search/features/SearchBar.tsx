96f0148d917a7676547abaaf7ed17349415f7175
seba-the-programmer <megaseba9@gmail.com>
2023-09-30 14:34:58 +0200

    Fix searchbar styling

--- frontend/src/components/main/Search/features/SearchBar.tsx
+++ frontend/src/components/main/Search/features/SearchBar.tsx
@@ -9,18 +9,19 @@ interface IProps {
 
 const SearchBar = ({ handler, value }: IProps) => {
   return (
-    <div className="w-full flex items-center justify-center">
-      <div className="px-4 rounded-l-xl py-2.5 border-2 dark:border-white-light transition-colors border-slate-700 border-r-0 dark:bg-black bg-white-light">
-        🔍
-      </div>
+    <div className="w-full flex flex-row-reverse items-center justify-center">
       <input
         type="text"
         value={value}
         onChange={handler}
         name="search-input"
         placeholder="Search for quests..."
-        className="input max-w-2xl py-2 pl-2 rounded-l-none"
+        className="input max-w-2xl py-2 pl-2 rounded-l-none border-l-0 peer"
       />
+      <div className="pl-3 pr-1 rounded-l-xl py-2.5 border-2 peer-focus:border-green-500 dark:peer-focus:border-accent dark:border-white-light transition-colors border-slate-700 border-r-0 dark:bg-black-light bg-white">
+        🔍
+      </div>
+
     </div>
   )
 }
